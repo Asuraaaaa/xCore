0f8ea5a98c02276c5afcd43a91b3fbfaf7f8ccec
xogos <xogos1997@gmail.com>
2020-02-08 22:55:54 +0100

    master added

---
+++ xCoreMaster/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+client_scripts {
+	"xCoreClient.net.dll",
+}
+
+server_script {	
+    "xCoreServer.net.dll",
+} 
---
+++ xMenuMaster/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+games { 'gta5' }
+
+client_scripts {
+	"xMenu.net.dll",
+}
+
+server_script {	
+	"xMenuServer.net.dll",
+} 
