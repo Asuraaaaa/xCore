1b7f54e0e611ce1707409e7673fc200375931285
xogos <xogos1997@gmail.com>
2020-02-08 21:57:34 +0100

    teleport
    added simple teleport to location, inspired by esx

---
+++ xCoreClient/Main/Player/functions/teleport.cs
@@ -0,0 +1,25 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System.Threading.Tasks;
+
+namespace xCoreClient.Main.Player.teleport
+{
+    public class teleport : BaseScript
+    {
+        [EventHandler("xCore:Client:teleport")]
+        public static async Task teleportPlayer(int entity,Vector3 vec,float heading = -999)
+        {
+            if(API.DoesEntityExist(entity))
+            {
+                API.RequestCollisionAtCoord(vec.X,vec.Y,vec.Z);
+                while(!API.HasCollisionLoadedAroundEntity(entity))
+                {
+                    await Delay(50);
+                }
+                API.SetEntityCoords(entity, vec.X, vec.Y, vec.Z, false, false, false, false);
+                if (heading == -999) API.SetEntityHeading(entity, API.GetEntityHeading(entity));
+                else API.SetEntityHeading(entity, heading);
+            }
+        }
+    }
+}
